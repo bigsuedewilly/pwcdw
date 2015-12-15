436eff5937116ecdf2dcec30bbb7103de00e4742
bigsuedewilly <cranedoesntwork@gmail.com>
2015-12-15 14:59:35 +0000

    Random Stuff

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
+var a = "active";
+
+
 $(".navElements").on("click", function() 
 {
-	if ($(this).hasClass("active"))
+	//Entering menu mode
+	
+	$("#menu").addClass(a);
+	$("#content").addClass(a);
+
+	//Need this function to be rewritten to squash a bug
+	setTimeout(function addButton()
 	{
-		$(this).removeClass("active");
-		$("#menu").removeClass("active");
-		$("#content").removeClass("active");
-	}
-	else 
-	{
-		$(this).addClass("active");
-		$("#menu").addClass("active");
-		$("#content").addClass("active");
-	}
+		$("#menu img").addClass(a);
+	}, 1000);
+
+	//Tab switching
+	var menuShow = $(this).attr("rel");
+	
+	$(".navElements.active").not(this).removeClass(a);
+	$(this).addClass(a);
+
 });
+
+//Exiting menu mode
+$("#close").on("click", function() {
+	$("#close").removeClass(a);
+	$("#menu").removeClass(a);
+	$("#content").removeClass(a);
+	$(".navElements").removeClass(a);
+});
+
+
+
